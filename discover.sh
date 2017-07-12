f46fc3355b199b74d6ffb3016c9a7ae206b7addd
Ricky Wilson <echoquote@gmail.com>
2017-07-12 14:21:52 -0500

    Update discover.sh
    Changed the formatting

--- discover.sh
+++ discover.sh
@@ -3,17 +3,17 @@
 nhosts=$1
 
 nmap \
---host-timeout 10s \
---max-retries 0 \
-–min-parallelism 100 \
--T5 \
--Pn \
--n \
--iR $1 \
--p80 \
--oG tmp.txt \
---open \
-tmp.txt > /dev/null
+    --host-timeout 10s \
+    --max-retries 0 \
+    –min-parallelism 100 \
+    -T5 \
+    -Pn \
+    -n \
+    -iR $1 \
+    -p80 \
+    -oG tmp.txt \
+    --open \
+    tmp.txt > /dev/null
 
 cat tmp.txt | grep 'Host' | awk '{print $2}'
 rm tmp.txt
