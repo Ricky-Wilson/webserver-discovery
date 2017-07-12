115261d270d89f979b6e6cfb2b3307016a079662
Ricky Wilson <echoquote@gmail.com>
2017-07-12 15:03:57 -0500

    Update get_titles.py
    Improved documentation.
    I'm Now using reST style documentation.

--- get_titles.py
+++ get_titles.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
-""" Probe random web server and print out the title of their index page. """
+""" 
+Probe random web server and print out the title of their index page. 
+
+:param nhosts: The number of random IPs you want to probe. 
+"""
 
 import os
 import sys
+
+# third party imports
 import lxml.html
 
 
 def get_title(url):
-    '''Extract the title from a webpage.
-    If anything goes wrong like the request
-    times out or the page does not have a
-    title get_title will return None'''
+    """
+    Extract the title from a web page..
+
+    :param url: The URL of the page
+    :returns: A string if title was found or None if somthing goes wrong
+    """
     try:
         title = lxml.html.parse(url)
         return title.find(".//title").text
@@ -21,16 +29,26 @@ def get_title(url):
         return None
 
 def shell(command):
-    """Run a shell command.
-    return it's output as a string"""
+    """
+    Execute a shell command and return it's output.
+    
+    :param command: The command you want to execute
+    :returns: A string representation of the commands output
+    """
     return os.popen(command).read()
 
 def scan(nhosts):
-    """Run an nmap scan of n hosts.
-    This will return IPv4 addresses of hosts
-    that are up and have a service running on
-    port 80. Simply supply the number of hosts
-    you would like to check."""
+    """
+    Scan for web servers.
+    
+    This function uses namp to find random systems 
+    that are running a service on port 80.
+    We will assume that if the system is running a service
+    on port 80 it's a web server and generate a URL for it.
+    
+    :param nhosts:  The number of random IPs you would like to probe
+    :returns: A list if URLs
+    """
     results = set(shell('./discover.sh {}'.format(nhosts)).split('\n'))
     if not results:
         print 'Nothing Found.'
@@ -38,7 +56,10 @@ def scan(nhosts):
     return ["http://{}".format(ip) for ip in results if ip]
 
 def main():
-    """ Run the scan."""
+    """  
+    This function puts everything together    
+    :returns: None
+    """
     if not len(sys.argv) >= 2:
         print 'You forgot to tell me how many hosts to check'
         sys.exit(0)
