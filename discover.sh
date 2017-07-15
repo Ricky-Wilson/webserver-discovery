520e4e3ce495241b1805438def774b4006c93a04
Ricky Wilson <echoquote@gmail.com>
2017-07-15 15:05:40 -0500

    Update discover.sh
    Removed unused variable nhosts.
    Fixed indentation.
    Shortened the parsing portion to just awk.

--- discover.sh
+++ discover.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-nhosts=$1
-
 nmap \
---host-timeout 10s \
---max-retries 0 \
-–min-parallelism 100 \
--T5 \
--Pn \
--n \
--iR $1 \
--p80 \
--oG tmp.txt \
---open \
-tmp.txt > /dev/null
-
-cat tmp.txt | grep 'Host' | awk '{print $2}'
-rm tmp.txt
+    --host-timeout 10s \
+    --max-retries 0 \
+    –min-parallelism 100 \
+    -T5 \
+    -Pn \
+    -n \
+    -iR "$1" \
+    -p80 \
+    -oG tmp.txt \
+    --open \
+    tmp.txt > /dev/null
+    awk '/Host/ {print $2}' tmp.txt
+    rm tmp.txt
